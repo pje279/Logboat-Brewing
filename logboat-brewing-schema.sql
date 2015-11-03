4d049baf6a01dd2eb239fc168fd20bc4c7acef02
Jacob Muchow <jacobamuchow@gmail.com>
2015-11-03 08:36:33 -0600

    Added ingredient entity to SQL dump.

--- logboat-brewing-schema.sql
+++ logboat-brewing-schema.sql
@@ -1,9 +1,24 @@
 DROP TABLE IF EXISTS user;
 
 CREATE TABLE user (
-	id smallint AUTO_INCREMENT PRIMARY KEY,
-	username varchar(15) UNIQUE,
-	password varchar(20)
+	id smallint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	username varchar(15) NOT NULL UNIQUE,
+	password varchar(20) NOT NULL
 );
 
 INSERT INTO user VALUES (DEFAULT, 'test', 'pass');
+
+
+DROP TABLE IF EXISTS ingredient;
+
+CREATE TABLE ingredient (
+	id smallint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name varchar(30) NOT NULL,
+	supplier varchar(30) DEFAULT NULL,
+	quantity float NOT NULL DEFAULT 0,
+	units ENUM('oz', 'mL', 'L', 'lbs')
+);
+
+INSERT INTO ingredient VALUES
+	(DEFAULT, 'Salt', 'SaltCo', DEFAULT, 'oz'),
+	(DEFAULT, 'Pepper', 'Pepper Inc.', 15.63, 'oz');
