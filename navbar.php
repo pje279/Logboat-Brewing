9f106ff55f77dc0210f7746f9fc501d9044fb053
Devun Schmutzler <devun.mac@gmail.com>
2015-11-04 12:31:57 -0600

    Revert "fixed collapsable navbar issue with jquery"
    This reverts commit e13c0d6421ec1bb2c9a460b8a7706e3ef409e1cd.

--- navbar.php
+++ navbar.php
@@ -6,8 +6,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <title>Logboat Brewing</title>
+<title>Logboat Brewing</title>
 </head>
 <body>
 <nav class="navbar navbar-default">
