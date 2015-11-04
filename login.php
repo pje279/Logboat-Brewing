f8f14c58bda8f755951c4e62c216a3487a82ad6e
percyodi <pearse.2008@gmail.com>
2015-11-04 00:06:10 +0000

    Work on legin.php

--- login.php
+++ login.php
@@ -1,5 +1,34 @@
 <?php
 
-echo "Test login page";
+session_start();
+
+if(isset($_SESSION['userId'])){
+    header("Location: start.php");
+    exit;
+}
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Logboat Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <form>
+                    <h2 class="text-center">Please Sign In</h2>
+                    <label for='inputUsername' class="sr-only">Email Address</label>
+                    <input type="text" id="inputUsername" name="inputUsername" class="form-control input-lg" placeholder="Username" required autofocus>
+                    <label for='inputpassword' class="sr-only">Password</label>
+                    <input type="password" id="inputpassword" name="inputpassword" class="form-control input-lg" placeholder="Password" required>
+                    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign In</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
