5e8de290a66e13702f7c26ec601f79e1e0db7cad
percyodi <pearse.2008@gmail.com>
2015-11-30 18:33:26 +0000

    Working on ingredient/create.php

--- ingredient/create.php
+++ ingredient/create.php
@@ -1,5 +1,5 @@
 <?php
-require '../api/init.php';
+require '../utilities/init.php';
 require '../utilities/tools.php';
 
 ?>
@@ -10,9 +10,20 @@ require '../utilities/tools.php';
     </head>
     <body>
         <p>Tools test</p>
-        <form method="post" action=""
-        <?php
-            say("hurray");
-        ?>
+        <form method="post" action="<?php echo getBaseUrl(); ?>api/ingredient/create.php">
+            <div class="form-group">
+                <label for="name">Ingredient Name</label>
+                <input type="text" class="form-control" id="name" name="name" required>
+            </div>
+            <div class="form-group">
+                <label for="supplier">Supplier</label>
+                <input type="text" class="form-control" id="supplier" name="supplier">
+            </div>
+            <div class="form-group">
+                <label for="quantity">Quantity</label>
+                <input type="number" class="form-control" id="quantity" name="quantity" value='0' required>
+            </div>
+            <button type="submit" class="btn btn-default pull-right">Add Ingredient</button>
+        </form>
     </body>
 </html>
