1600bac446edd37cec7d65cb42ba8f261888fbb2
percyodi <pearse.2008@gmail.com>
2015-11-30 17:43:12 +0000

    Added Content-type to api/init.php

--- api/init.php
+++ api/init.php
@@ -16,4 +16,6 @@ $cloudApp = "us-cdbr-azure-central-a.cloudapp.net";
 //Master VM
 $link = mysqli_connect($cloudApp, "be1dbd64a86c89", "3b83625d", "logboatdb") or fail("Connection error " . mysql_error($link));
 
+header('Content-type: application/json');
+
 session_start();
