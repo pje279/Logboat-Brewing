f43b8e2b4d87ef9d9b336eba6b42a47819138b01
percyodi <pearse.2008@gmail.com>
2015-11-30 23:50:08 +0000

    Moved new ingredient to a modal. Added jQuery animations and loading icons

---
+++ ingredient/createModal.php
@@ -0,0 +1,31 @@
+<?php
+require '../utilities/init.php';
+require '../utilities/tools.php';
+
+?>
+<form id="createIngredientForm" method="post" action="<?php echo getBaseUrl(); ?>api/ingredient/create.php">
+    <div id="errorMessage" class="alert alert-danger text-center" role="alert" style="display: none;"></div>
+    <div class="form-group">
+        <label for="name">Ingredient Name</label>
+        <input type="text" class="form-control" id="name" name="name" maxlength="100" required>
+    </div>
+    <div class="form-group">
+        <label for="supplier">Supplier</label>
+        <input type="text" class="form-control" id="supplier" name="supplier" maxlength="30">
+    </div>
+    <div class="form-group">
+        <label for="quantity">Quantity</label>
+        <input type="number" class="form-control" id="quantity" name="quantity" value='0' required>
+    </div>
+    <div class="form-group">
+        <label for="unitId">Units</label>
+        <select name="unitId" class="form-control">
+            <?php 
+            $units = Database::runQuery("SELECT * FROM unit");
+            foreach($units as $unit) {
+                echo "<option value='{$unit['id']}'>{$unit['name']}</option>";
+            }
+            ?>
+        </select>
+    </div>
+</form>
--- ingredient/showAll.php
+++ ingredient/showAll.php
@@ -10,6 +10,7 @@ require '../utilities/tools.php';
         
         <script>
             $(document).ready(function() {
+                //Get all of the rows
                 $.get("../api/ingredient/getAll.php", function(data) {
                     $("#showAllLoading").fadeOut("slow", function() {
                         for(var i = 0, len = data.result.length; i < len; i++) {
@@ -27,33 +28,91 @@ require '../utilities/tools.php';
                         $("#getAllTable tr").each(function() {
                             $(this).click(function() {
                                 console.log("Clicked");
+                                $("#updateModal .modal-body").html("<div style='text-align: center;'><i class='fa fa-circle-o-notch fa-spin fa-5x text-center'></i></div>");
+                                $("#updateModal").modal('toggle');
                                 $.get("updateModal.php", {"ingredientId": $(this).attr("data-ingredientId")}, function(data) {
-                                    $("#showAllModal .modal-body").html(data);
-                                    $("#showAllModal").modal('toggle');
+                                    $("#updateModal .modal-body div").fadeOut("slow", function() {
+                                        $("#updateModal .modal-body").hide().html(data).slideDown("slow");
+                                    });
                                 });
                             });
                         });
                     });
                     
                 });
-                // Set modal buttons
-                $("#modalSave").click(function() {
-                    $.post("../api/ingredient/update.php", $("#updateIngredientForm").serialize(), function(jsonData) {
-                        if(jsonData.success === false) {
-                            $("#errorMessage")
-                                .html(jsonData.error)
-                                .slideDown("fast")
-                                .delay(10000)
-                                .slideUp(1000);
-                        } else {
-                            window.location = "../ingredient/showAll.php";
-                        }
+                
+                //Set Create Modal links
+                $(".callCreateModal").click(function(e) {
+                    e.preventDefault();
+                    $("#createModal .modal-body").html("<div style='text-align: center;'><i class='fa fa-circle-o-notch fa-spin fa-5x text-center'></i></div>");
+                    $("#createModal").modal('toggle');
+                    $.get("createModal.php", function(data) {
+                        $("#createModal .modal-body div").fadeOut("slow", function() {
+                            $("#createModal .modal-body").hide().html(data).slideDown("slow");
+                        });
                     });
                 });
                 
-                $("#modalDelete").click(function() {
+                // Set modal buttons
+                $(".modalSave").click(function() {
+                    if($("#updateIngredientForm input#name").val() == '') {
+                        $("#errorMessage")
+                            .html("Please Enter an Ingredient Name")
+                            .slideDown("fast")
+                            .delay(10000)
+                            .slideUp(1000);
+                    } else if ($("#updateIngredientForm input#name").val().length > 100) {
+                        $("#errorMessage")
+                            .html("Ingredient Name can be no longer than 100 characters")
+                            .slideDown("fast")
+                            .delay(10000)
+                            .slideUp(1000);
+                    } else {
+                        $.post("../api/ingredient/update.php", $("#updateIngredientForm").serialize(), function(jsonData) {
+                            if(jsonData.success === false) {
+                                $("#errorMessage")
+                                    .html(jsonData.error)
+                                    .slideDown("fast")
+                                    .delay(10000)
+                                    .slideUp(1000);
+                            } else {
+                                window.location = "../ingredient/showAll.php";
+                            }
+                        });
+                    }
+                });
+                
+                $(".modalDelete").click(function() {
                     if(confirm("Are you sure you want to delete this ingredient? This is not reversable!")) {
                         $.post("../api/ingredient/delete.php", {"id":$("#updateIngredientForm > #ingredientId").val()} , function(jsonData) {
+                            if(jsonData.success === false) {
+                                $("#errorMessage")
+                                    .html(jsonData.error)
+                                    .slideDown("fast")
+                                    .delay(10000)
+                                    .slideUp(1000);
+                            } else {
+                                window.location = "../ingredient/showAll.php";
+                            }
+                        });
+                    }
+                });
+                
+                $(".modalCreate").click(function() {
+                    if($("#createIngredientForm input#name").val() == '') {
+                        $("#errorMessage")
+                            .html("Please Enter an Ingredient Name")
+                            .slideDown("fast")
+                            .delay(10000)
+                            .slideUp(1000);
+                    } else if ($("#createIngredientForm input#name").val().length > 100) {
+                        $("#errorMessage")
+                            .html("Ingredient Name can be no longer than 100 characters")
+                            .slideDown("fast")
+                            .delay(10000)
+                            .slideUp(1000);
+                    } else {
+                        $.post("../api/ingredient/create.php", $("#createIngredientForm").serialize() , function(jsonData) {
                             if(jsonData.success === false) {
                                 $("#errorMessage")
                                     .html(jsonData.error)
@@ -73,7 +132,7 @@ require '../utilities/tools.php';
         <?php require '../navbar.php'; ?>
         <div class="container">
             <div class="row">
-                <a href="<?php echo getBaseUrl(); ?>ingredient/create.php">Add a New Ingredient</a>
+                <a href="<?php echo getBaseUrl(); ?>ingredient/create.php" class="callCreateModal">Add a New Ingredient</a>
                 <table id="getAllTable" class="table table-hover">
                 <?php
                 
@@ -82,25 +141,40 @@ require '../utilities/tools.php';
                 
                 ?>
                 </table>
-                <div id="showAllLoading" class="col-xs-2 col-xs-offset-5"><i class="fa fa-circle-o-notch fa-spin fa-5x text-center"></i></div>
+                <div id="showAllLoading" style="text-align: center;"><i class="fa fa-circle-o-notch fa-spin fa-5x text-center"></i></div>
             </div>
         </div>
         
         <!--Modal Used for clicking on a row-->
-        <div class="modal fade" id="showAllModal" tabindex="-1" role="dialog" aria-labelledby="showAllModal">
+        <div class="modal fade" id="updateModal" tabindex="-1" role="dialog" aria-labelledby="updateModal">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                         <h4 class="modal-title" id="modalLabel">Update Ingredient Form</h4>
                     </div>
-                    <div class="modal-body">
-                        <p>Text</p>
-                    </div>
+                    <div class="modal-body"></div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">Close Without Saving</button>
-                        <button id="modalDelete" type="button" class="btn btn-danger">Delete Ingredient</button> 
-                        <button id="modalSave" type="button" class="btn btn-primary">Save changes</button>
+                        <button type="button" class="modalDelete btn btn-danger">Delete Ingredient</button> 
+                        <button type="button" class="modalSave btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <!--Modal Used for new ingredient-->
+        <div class="modal fade" id="createModal" tabindex="-1" role="dialog" aria-labelledby="createModal">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="modalLabel">New Ingredient Form</h4>
+                    </div>
+                    <div class="modal-body"></div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close Without Saving</button>
+                        <button type="button" class="modalCreate btn btn-primary">Create Ingredient</button>
                     </div>
                 </div>
             </div>
