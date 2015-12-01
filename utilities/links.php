5434ec8e68584a41f8280adce4f8fc05784d46a8
Jacob Muchow <jacobamuchow@gmail.com>
2015-12-01 15:12:47 -0600

    Added getBaseUrl() to styles so styles are actually everywhere.

--- utilities/links.php
+++ utilities/links.php
@@ -10,4 +10,5 @@
 <!-- Bootstrap JavaScript (required JQuery) -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
 
-<link rel="stylesheet" href="styles.css?version=0.0.1">
+<!-- Logboat CSS -->
+<link rel="stylesheet" href="<?= getBaseUrl(); ?>styles.css?version=0.0.1">
