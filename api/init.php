18f1efe9f9c3c219e25536da5fb2bf21daa13f26
percyodi <pearse.2008@gmail.com>
2015-12-01 14:33:03 +0000

    Fixed broken api/schedule/getAll.php. Schema change broke the script, but it's back up and running now

--- api/init.php
+++ api/init.php
@@ -3,6 +3,6 @@
 //Master VM
 $link = mysqli_connect("us-cdbr-azure-central-a.cloudapp.net", "be1dbd64a86c89", "3b83625d", "logboatdb") or fail("Connection error " . mysql_error($link));
 
-//header('Content-type: application/json');
+header('Content-type: application/json');
 
 session_start();
