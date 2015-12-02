3b309c178082f13e9a32e3307ba192139cb08884
Seth vonSeggern <savtk5@mail.missouri.edu>
2015-12-02 11:49:09 -0600

    select brew to show fermentation graph

---
+++ analytics/selectBrew.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+  <form id="selectBrewFermentation" method="post" action="<?php echo getBaseUrl(); ?>api/analytics/select.php">
+  <div class="form-group">
+        <label for="beerId">Beer</label>
+        <select name="beerId" class="form-control">
+          <?php 
+            $beers = Database::runQuery("SELECT * FROM beer");
+            foreach($beers as $beer) {
+              echo "<option value='{$beer['id']}'>{$beer['name']}</option>";
+            }
+          ?>
+        </select>
+        <select name="fermentationType" class="form-control">
+          <?php
+            $types = Database::runQuery("SELECT * from fermentationType");
+            foreach($types as $type) {
+              echo "<option value='{$type['id']}'>{$type['name']}</option>";
+            }
+          ?>
+        </select>
+        <button type="button" class="btn btn-primary">Submit</button>
+  </div>
+  </form>
+</body>
+</html>
