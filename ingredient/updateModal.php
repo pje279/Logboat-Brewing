c5eebf9dc4615205beef5976ea47dc6452db67d6
Seth vonSeggern <savtk5@mail.missouri.edu>
2015-12-07 13:07:20 -0600

    added low value to update modal

--- ingredient/updateModal.php
+++ ingredient/updateModal.php
@@ -37,4 +37,8 @@ $data = $data[0]; // Grab the first result (should only be one)
             ?>
         </select>
     </div>
+    <div class="form-group">
+        <label for="quantity">Low Value</label>
+        <input type="number" class="form-control" id="lowValue" name="lowValue" value='<?php echo $data['lowValue'] ?>' required>
+    </div>
 </form>
