aa87905334d081a7d381dbac22c5c2ab7ea0dfa5
Seth vonSeggern <savtk5@mail.missouri.edu>
2015-12-07 12:51:13 -0600

    added low value to insert query

--- api/ingredient/create.php
+++ api/ingredient/create.php
@@ -7,16 +7,17 @@ $name = htmlspecialchars($_POST['name']);
 $supplier = htmlspecialchars($_POST['supplier']);
 $quantity = $_POST['quantity'];
 $unitId = $_POST['unitId'];
+$lowIngredient = $_POST['low'];
 
 //TODO: handle cases for empty supplier / quantity
 
 /* This is the old query. Current schema doesn't support $unitId */
-$query = 'INSERT INTO ingredient VALUES (DEFAULT, ?, ?, ?, ?)';
+$query = 'INSERT INTO ingredient VALUES (DEFAULT, ?, ?, ?, ?, ?)';
 
 
 $stmt = $link->prepare($query);
 
-$stmt->bind_param("ssdd", $name, $supplier, $quantity, $unitId);
+$stmt->bind_param("ssdd", $name, $supplier, $quantity, $unitId, $lowIngredient);
 
 if($stmt->execute()) {
     success();
